92603ef687e9eb7debdd520b1b7abda2675bb3d9
KauanDalat <kauan252522r@gmail.com>
2026-05-28 22:19:04 -0300

    Testando

---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+printf("Olá, CI do GitHub!\n");
+return 0;
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <assert.h>
+#include <stdio.h>
+int main() {
+// Validação de lógica simples
+assert(1 == 1);
+printf("Teste passou com sucesso!\n");
+return 0;
+}
